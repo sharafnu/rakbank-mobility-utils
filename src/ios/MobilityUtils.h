0d3a0b63e1d5696d46b3c53cc5c1bfd16fb4fb82
Sharaf <sharaf.innovazions@gmail.com>
2018-07-26 17:41:07 +0400

    iOS implementation by Rama

--- src/ios/MobilityUtils.h
+++ src/ios/MobilityUtils.h
@@ -1,10 +1,11 @@
 #import <Cordova/CDVPlugin.h>
 
-@interface MobilityUtils : CDVPlugin {
-}
+@interface MobilityUtils : CDVPlugin 
+
 
 // The hooks for our plugin commands
 - (void)echo:(CDVInvokedUrlCommand *)command;
 - (void)getDate:(CDVInvokedUrlCommand *)command;
 - (void)loadProps:(CDVInvokedUrlCommand *)command;
+
 @end
--- src/ios/MobilityUtils.m
+++ src/ios/MobilityUtils.m
@@ -32,4 +32,82 @@ static NSString* const kCryptIv = @"";
   [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
+
+
+- (void)loadProps:(CDVInvokedUrlCommand *)command{
+
+    CDVPluginResult* pluginResult = nil;
+     NSString *wwwPath = [[NSBundle mainBundle].resourcePath stringByAppendingString:@"/www/assets/props.json"];
+    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat: @"%@",wwwPath]];
+
+   // if ([[self class] checkCryptFile:url]) {
+        NSString *mimeType = [self getMimeType:url];
+        NSError* error;
+        NSString* content = [[NSString alloc] initWithContentsOfFile:url.path encoding:NSUTF8StringEncoding error:&error];
+        if (!error) {
+            NSData* data = [self decryptAES256WithKey:kCryptKey iv:kCryptIv data:content];
+            NSString *formattedResult = [NSString stringWithUTF8String:[data bytes]];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:formattedResult];
+        }else{
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+            NSLog(@"Errorr in decrypting data :: %@", error);
+        }
+         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    //}
+}
+
+- (NSData *)decryptAES256WithKey:(NSString *)key iv:(NSString *)iv data:(NSString *)base64String {
+     
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:base64String options:0];
+    size_t bufferSize = [data length] + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+    size_t numBytesDecrypted = 0;
+    
+    NSData *keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *ivData = [iv dataUsingEncoding:NSUTF8StringEncoding];
+    
+    CCCryptorStatus status = CCCrypt(kCCDecrypt,
+                                     kCCAlgorithmAES128,
+                                     kCCOptionPKCS7Padding,
+                                     keyData.bytes,
+                                     kCCKeySizeAES256,
+                                     ivData.bytes,
+                                     data.bytes,
+                                     data.length,
+                                     buffer,
+                                     bufferSize,
+                                     &numBytesDecrypted);
+    
+    if (status == kCCSuccess) {
+        return [NSData dataWithBytes:buffer length:numBytesDecrypted];
+    }
+    free(buffer);
+    return nil;
+}
+
+- (NSString*)getMimeType:(NSURL *)url
+{
+    NSString *fullPath = url.path;
+    NSString *mimeType = nil;
+    if (fullPath) {
+        CFStringRef typeId = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)[fullPath pathExtension], NULL);
+        if (typeId) {
+            mimeType = (__bridge_transfer NSString*)UTTypeCopyPreferredTagWithClass(typeId, kUTTagClassMIMEType);
+            if (!mimeType) {
+                // special case for m4a
+                if ([(__bridge NSString*)typeId rangeOfString : @"m4a-audio"].location != NSNotFound) {
+                    mimeType = @"audio/mp4";
+                } else if ([[fullPath pathExtension] rangeOfString:@"wav"].location != NSNotFound) {
+                    mimeType = @"audio/wav";
+                } else if ([[fullPath pathExtension] rangeOfString:@"css"].location != NSNotFound) {
+                    mimeType = @"text/css";
+                }
+            }
+            CFRelease(typeId);
+        }
+    }
+    return mimeType;
+}
+
+
 @end
